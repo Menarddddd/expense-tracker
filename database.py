41f8ab804ec669120ac65283c3cdbe616df8df29
menard <menardddddd@gmail.com>
2026-02-11 18:27:48 +0800

    configured database

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///database.db"
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
